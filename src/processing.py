4e26019cbaac37c54c39b36ed3f829130893052e
Boldina Elena <s35011111@gmail.com>
2025-06-10 15:14:05 +0300

    added changes to processing

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,16 @@
 from typing import Iterable, Optional
 
 
-def filter_by_state(data: Optional, state_key: str = "EXECUTED") -> Iterable:
+def filter_by_state(dictionary_list: Optional, state_key: str = "EXECUTED") -> Iterable:
     """принимает список словарей возвращает новый список словарей, содержащий только те словари,
     у которых ключ state_key соответствует указанному значению"""
-    result = []
-    for item in range(len(data)):
-        if data[item]["state"] == state_key:
-            result.append(data[item])
-    return result
+    dictionary_list_result = []
+    for item in range(len(dictionary_list)):
+        if dictionary_list[item]["state"] == state_key:
+            dictionary_list_result.append(dictionary_list[item])
+    return dictionary_list_result
 
 
-def sort_by_date(data: Optional, sort_order: bool = True) -> Iterable:
+def sort_by_date(dictionary_list: Optional, sort_order: bool = True) -> Iterable:
     """Функция возвращает новый список, отсортированный по дате"""
-    return sorted(data, key=lambda x: x["date"], reverse=sort_order)
+    return sorted(dictionary_list, key=lambda x: x["date"], reverse=sort_order)
