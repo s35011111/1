a9fe220e34e605896027b08b6120e9f09b0101c6
Boldina Elena <s35011111@gmail.com>
2025-06-15 22:43:10 +0300

    edited generators

--- src/generators.py
+++ src/generators.py
@@ -1,30 +1,22 @@
 from typing import Iterable, Any, Iterator
 
 
-def filter_by_currency(dictionary_transactions: Iterable, currency: str) -> Iterator:
+def filter_by_currency(dictionary_transactions: Iterable[dict], currency: str) -> Iterator[dict]:
     """Функция принимающая на вход список словарей, представляющих транзакциии возвращающая итератор,
     который поочередно выдает транзакции, где валюта операции соответствует заданной"""
-    for i in dictionary_transactions:
-        if i["operationAmount"]["currency"]["code"] == currency:
-            yield i
-    while True:
+    for
         yield {}
 
 
-def transaction_descriptions(dictionary_transactions: Iterable) -> Iterator:
+def transaction_descriptions(dictionary_transactions: Iterable[dict]) -> Iterator[dict]:
     """Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
-    for i in dictionary_transactions:
-        yield i["description"]
-    while True:
+
         yield ""
 
 
-def card_number_generator(start: int, end: int) -> Any:
+def card_number_generator(start: int, end: int) -> str:
     """Функция выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
     for i in range(start, end + 1):
         width = 16
-        card = str(i).zfill(width)
-        block_size = 4
-        yield " ".join(card[i : i + block_size] for i in range(0, width, block_size))
-    while True:
+
         yield ""
