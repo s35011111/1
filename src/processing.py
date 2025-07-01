a977b4c8579a388354da1b2b52ae4a56d163cc81
Boldina Elena <s35011111@gmail.com>
2025-07-01 13:53:27 +0300

    files for HW13_2 changes for processing

--- src/processing.py
+++ src/processing.py
@@ -4,13 +4,17 @@ from typing import Any
 def filter_by_state(dictionary_list: "Any", state_key: str = "EXECUTED") -> Any:
     """принимает список словарей возвращает новый список словарей, содержащий только те словари,
     у которых ключ state_key соответствует указанному значению"""
-    dictionary_list_result = []
-    for item in range(len(dictionary_list)):
-        if dictionary_list[item]["state"] == state_key:
-            dictionary_list_result.append(dictionary_list[item])
-    return dictionary_list_result
+    return list(filter(lambda x: x["state"] == state_key, dictionary_list))
 
 
 def sort_by_date(dictionary_list: "Any", sort_order: bool = True) -> Any:
     """Функция возвращает новый список, отсортированный по дате"""
     return sorted(dictionary_list, key=lambda x: x["date"], reverse=sort_order)
+
+
+def filter_by_currensy(dictionary_list: "Any") -> Any:
+    return list(filter(lambda x: x["currency_code"] == "RUB", dictionary_list))
+
+
+def filter_by_currensy_json(dictionary_list: "Any") -> Any:
+    return list(filter(lambda x: x["operationAmount"]["currency"]["code"] == "RUB", dictionary_list))
